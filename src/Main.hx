74abcb6528678690402b0c480e8cce95d7772bb7
Zack Campbell <zajrik@gmail.com>
2017-02-19 20:39:18 -0600

    Push current code

--- src/Main.hx
+++ src/Main.hx
@@ -1,13 +1,13 @@
 import oc.Robot;
-import oc.Sides;
 
 class Main
 {
 	public static function main(): Void
 	{
+		Robot.turnLeft();
+
 		var func: String -> String -> String = untyped __lua__("function(a, b, ...) return a, b, ... end");
 		var bar: Dynamic = Util.wrap(func, 'foo', 'bar', 3, 4, 5, 6);
 		trace(bar);
-		Robot.place(Sides.bottom);
 	}
 }
--- src/Util.hx
+++ src/Util.hx
@@ -4,11 +4,11 @@ class Util
 {
 	/**
 	 * Serves as a wrapper for Lua methods with multiple return values,
-	 * returning a numberically indexed lua table containing the return
+	 * returning a numerically indexed lua table containing the return
 	 * values.
 	 * 
-	 * Must receive an uncalled Lua method, followed by any
-	 * args to be passed to the method
+	 * Must receive a method that returns multiple values, followed
+	 * by any parameters to be passed to that method
 	 */
 	public static var wrap: Dynamic -> Rest<Dynamic> -> Dynamic = untyped __lua__(
 		'function(f, ...) return { f(...) } end');
