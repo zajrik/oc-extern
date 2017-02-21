cc57d29487630a5fbb6c8a0b72e8f0182ff8d299
Zack Campbell <zajrik@gmail.com>
2017-02-20 18:12:31 -0600

    Update types for Util.wrap

--- src/Util.hx
+++ src/Util.hx
@@ -1,4 +1,6 @@
+import haxe.Constraints.Function;
 import haxe.extern.Rest;
+import lua.Table;
 
 class Util
 {
@@ -10,6 +12,6 @@ class Util
 	 * Must receive a method that returns multiple values, followed
 	 * by any parameters to be passed to that method
 	 */
-	public static var wrap: Dynamic -> Rest<Dynamic> -> Dynamic = untyped __lua__(
+	public static var wrap: Function -> Rest<Dynamic> -> Table<Int, Dynamic> = untyped __lua__(
 		'function(f, ...) return { f(...) } end');
 }
